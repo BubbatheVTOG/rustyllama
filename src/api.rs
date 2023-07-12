fc2211206fd1350b50b9cfc2e0148a5c3f80dc7b
Bubba <bubbathevtog@gmail.com>
2023-07-12 07:49:11 -0400

    init

---
+++ src/api.rs
@@ -0,0 +1,7 @@
+use leptos::*;
+use crate::model::conversation::Conversation;
+
+#[server(Converse "/api")]
+pub async fn converse(cx: Scope, prompt: Conversation) -> Result<String, ServerFnError> {
+
+}
---
+++ src/app.rs
@@ -0,0 +1,38 @@
+use leptos::*;
+use leptos_meta::*;
+// use leptos_router::*;
+
+use crate::model::conversation::{Conversation, Message};
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    // Provides context that manages stylesheets, titles, meta tags, etc.
+    provide_meta_context(cx);
+
+    let (conversation, set_conversation) = create_signal(cx, Conversation::new());
+
+    let send = create_action(cx, move |new_message: &String| {
+        let user_message = Message {
+            text: new_message.clone(),
+            user: true,
+        };
+        set_conversation.update(move |c| {
+            c.message.push(user_message);
+        });
+
+        // TODO: converse
+    });
+
+    view! { cx,
+        // injects a stylesheet into the document <head>
+        // id=leptos means cargo-leptos will hot-reload this stylesheet
+        <Stylesheet id="leptos" href="/pkg/leptos_start.css"/>
+
+        // sets the document title
+        <Title text="Welcome to Leptos"/>
+
+        // content for this welcome page
+        <ChatArea conversation />
+        <TypeArea send />
+    }
+}
